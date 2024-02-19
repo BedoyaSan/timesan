502277ac1ed3748f20b9e266c21aa70401c5cb7c
San <bedoyasan@outlook.com>
2024-02-19 09:39:01 -0500

    fix

--- lib/ui/info_screen.dart
+++ lib/ui/info_screen.dart
@@ -13,31 +13,27 @@ Widget infoScreen(BuildContext context, String message) {
       width: MediaQuery.of(context).size.width,
       color: Colors.black.withOpacity(0.7),
       child: Center(
-          child: Flexible(
-        fit: FlexFit.tight,
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Container(
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25),
-                    bottomRight: Radius.circular(25),
-                  ),
-                  color: Colors.black),
-              padding: const EdgeInsets.all(25),
-              child: Text(
-                message,
-                style: GoogleFonts.robotoCondensed(
-                  color: Colors.white,
-                  fontSize: 24,
-                  decoration: TextDecoration.none,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
                 ),
+                color: Colors.black),
+            padding: const EdgeInsets.all(25),
+            child: Text(
+              message,
+              style: GoogleFonts.robotoCondensed(
+                color: Colors.white,
+                fontSize: 24,
+                decoration: TextDecoration.none,
               ),
             ),
           ),
         ),
-      )),
+      ),
     ),
   );
 }
